d084c76dadd1d8464c2d40e163d14965bdf8cb78
Tom Walker <tpwalke2@gmail.com>
2022-08-20 05:11:31 -0400

    Expect camel-casing in deserialization

--- HttpFunctionGenerator/SourceProviders/SerializationSourceProvider.cs
+++ HttpFunctionGenerator/SourceProviders/SerializationSourceProvider.cs
@@ -15,7 +15,17 @@ public static class Json
 {{
     public static T Deserialize<T>(string value)
     {{
-        return JsonSerializer.Deserialize<T>(value);
+        return JsonSerializer.Deserialize<T>(
+            value,
+            new JsonSerializerOptions()
+            {{
+                DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters =
+                {{
+                    new JsonStringEnumConverter()
+                }}
+            }});
     }}
 
     public static string Serialize<T>(T input)
