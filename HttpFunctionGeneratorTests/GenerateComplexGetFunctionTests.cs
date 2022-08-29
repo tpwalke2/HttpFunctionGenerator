7b25b9062f74afafb73c2d5ec1ee52367877c894
Tom Walker <tpwalke2@gmail.com>
2022-08-29 11:55:29 -0400

    #3 add failing test for generating binding info

---
+++ HttpFunctionGeneratorTests/GenerateComplexGetFunctionTests.cs
@@ -0,0 +1,68 @@
+using HttpFunctionGenerator;
+using System.Linq;
+using Xunit;
+
+namespace HttpFunctionGeneratorTests;
+
+public class GenerateComplexGetFunctionTests
+{
+    private static readonly string Source = $@"using {Constants.PackageBaseName}.Attributes;
+using {Constants.PackageBaseName}.Models;
+using System.Threading.Tasks;
+
+namespace HttpFunctionGeneratorTest;
+
+public record ResourceQuery(uint Id);
+public record ResourceResult(string Name); 
+
+[HttpFunction]
+public class C {{
+    public Task<Outcome<ResourceResult>> GetResource(ResourceQuery q) {{
+        return Task.FromResult(new Outcome<ResourceResult>(Status.Created, new ResourceResult(""John Doe"")));
+    }}
+}}";
+    
+    [Fact]
+    public void ShouldNotHaveDiagnosticErrors()
+    {
+        var result = GeneratorTestFactory.RunGenerator(Source);
+        Assert.Empty(result.Diagnostics.After);
+    }
+
+    [Fact]
+    public void ShouldBuildGetFunction()
+    {
+        var expected = $@"using {Constants.PackageBaseName}.Mapping;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Threading.Tasks;
+
+namespace HttpFunctionGeneratorTest;
+
+public class C_Functions
+{{
+    private readonly C _controller;
+
+    public C_Functions(C controller)
+    {{
+        _controller = controller;
+    }}
+
+    [Function(""GetResource"")]
+    public async Task<HttpResponseData> RunAsync(
+        [HttpTrigger(
+            AuthorizationLevel.Function,
+            ""get"",
+            Route = null)] HttpRequestData req,
+        FunctionContext executionContext)
+    {{
+        var query = req.Bind<ResourceQuery>();
+        var outcome = await _controller.GetResource(query);
+        return await req.CreateResponse(outcome);
+    }}
+}}";
+        
+        var result = GeneratorTestFactory.RunGenerator(Source);
+        Assert.Equal(expected, result.RunResult.GeneratedTrees.First(t => t.FilePath.Contains("C_Functions")).GetText().ToString());
+    }
+}
